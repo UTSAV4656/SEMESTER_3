1bbc3672b731618c1275532c3c6b3fb2202689d8
UTSAV4656 <142557184+UTSAV4656@users.noreply.github.com>
2024-07-25 08:47:48 +0530

    Delete Lab_17 directory

--- Lab_17/ChildProcessDemo.js
+++
@@ -1,8 +0,0 @@
-const child_process = require('node:child_process');
-
-child_process.exec("Demo.java" , (err,stdout,stdin)=>{
-    if(err){
-    console.log(err);
-    }
-    console.log(stdout);
-});
